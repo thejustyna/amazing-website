889f75bbd9fff59f27ac741079a9e799e617ecbb
thejustyna <justynalezuchprogramista@gmail.com>
2017-05-08 16:01:14 +0200

    nav first elements

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php include("site-parts/1-basics/header.php"); ?>
 
+<?php include("site-parts/2-home-sections/nav.php"); ?>
+
 
 
 <?php include("site-parts/1-basics/end.php"); ?>
--- site-parts/2-home-sections/nav.php
+++ site-parts/2-home-sections/nav.php
@@ -1,10 +1,16 @@
 
-<section>
-    <h1 class="page-title">DevTips<br>Starter Kit</h1>
-    <p>Ground zero for your next project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate recusandae autem molestias, enim odio maxime doloribus. Blanditiis iusto illo, eius aperiam repellendus asperiores itaque culpa labore veniam porro unde quo libero veritatis dolorem facilis repudiandae adipisci natus. Nisi, consectetur. Ipsa asperiores officia harum nemo eveniet quas ad ullam tempora commodi.</p>
-
-    <a href="https://github.com/DevTips/DevTips-Starter-Kit/archive/master.zip" class="button button-download">Download Now</a>
-
-    <a class="fork-tag" href="https://github.com/DevTips/DevTips-Starter-Kit">Fork Me on GitHub</a>
-
-  </section>
+<header>
+	<h1 class="logo"> Logo name </h1>
+	<nav>
+		<ul>
+			<li><a href="#About">About</a></li>
+			<li><a href="#Youtube">Youtube</a></li>
+			<li><a href="#Mentoring">Mentoring</a></li>
+			<li><a href="#Articles">Articles</a></li>
+			<li><a href="#Vine">Vine</a></li>
+			<li><a href="#Design">Design</a></li>
+			<li><a href="#Notes">Notes</a></li>
+			<li><a href="#Contact">Contact</a></li>
+		</ul>
+	</nav>
+</header>
