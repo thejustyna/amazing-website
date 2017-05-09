9de4b744a3679da60558ee94e9e9de72c1e3f7d4
thejustyna <justynalezuchprogramista@gmail.com>
2017-05-09 13:19:36 +0200

    add pug files to my project

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include("site-parts/1-basics/header.php"); ?>
-
-<?php include("site-parts/2-home-sections/nav.php"); ?>
-
-
-
-<?php include("site-parts/1-basics/end.php"); ?>
-
--- site-parts/1-basics/end.php
+++
@@ -1,4 +0,0 @@
-<script src="js/jquery-2.1.4.min.js"></script>
-<script src="js/script.js" type="text/javascript"></script>
-</body>
-</html>
