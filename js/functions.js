5a9d09d58106e6f9fd890b8a1a8d023b12339be4
thejustyna <justynalezuchprogramista@gmail.com>
2017-05-18 12:56:42 +0200

    JavaScript mentoring-section
    Add a functions that move bubbles in mentoring-sections.

--- js/functions.js
+++ js/functions.js
@@ -2,19 +2,31 @@
  	mentoring_bubble_click();
  });
 
-function mentoring_bubble_click(){
-	$('.face').on('click', function(){
-		$(this).addClass('has-bubble-open')
-			.siblings().removeClass('has-bubble-open');
-	});
-}
+ function mentoring_bubble_click(){
+ 	$('.face').on('click', function(){
+
+ 		var $this = $(this);
+ 				face_distance_from_top_of_parent = $this.position().top,
+ 				vertMath = -(face_distance_from_top_of_parent - 200);
+
+ 		console.log(vertMath);
+
+ 		$this.parent().css('top', + vertMath + 'px');
+
+ 		$this.addClass('has-bubble-open')
+ 		.siblings().removeClass('has-bubble-open');
+ 	});
+
+
+
+ }
 
  $(window).scroll(function(){
  	scrolling_thumbnails_in_YouTube_section();
  });
 
-function scrolling_thumbnails_in_YouTube_section() {
-	var distance_from_browser_top = $(window).scrollTop();
+ function scrolling_thumbnails_in_YouTube_section() {
+ 	var scroll_position = $(window).scrollTop();
 
-	$('.video-strip').css('background-position', 'center '+ -distance_from_browser_top + 'px');
-}
+ 	$('.video-strip').css('background-position', 'center '+ -scroll_position + 'px');
+ }
