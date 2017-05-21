ec177aaaed2e7ab75c4e5ac9a1f4489c41b612b0
thejustyna <justynalezuchprogramista@gmail.com>
2017-05-21 15:35:34 +0200

    Mentoring JavaScript

--- js/functions.js
+++ js/functions.js
@@ -2,25 +2,39 @@
  	mentoring_bubble_click();
  });
 
+// ========= MENTORING SECTION ==========
+
  function mentoring_bubble_click(){
  	$('.face').on('click', function(){
 
- 		var $this = $(this);
+ 		var $this = $(this),
  				face_distance_from_top_of_parent = $this.position().top,
- 				vertMath = -(face_distance_from_top_of_parent - 200);
+ 				vert_distance_to_center = -(face_distance_from_top_of_parent - 200), 				
+ 				face_distance_from_left_of_parent = $this.position().left,
+ 				horiz_distance_to_left = -(face_distance_from_left_of_parent);
+// window.matchMedia("min-width: 640px").matches
+ 		if (window.matchMedia("(min-width: 640px)").matches) {
 
- 		console.log(vertMath);
+	 		$this.parent().css('top', + vert_distance_to_center + 'px');
+	 		$this.addClass('has-bubble-open');
 
- 		$this.parent().css('top', + vertMath + 'px');
+	 	} else {
 
- 		$this.addClass('has-bubble-open')
- 		.siblings().removeClass('has-bubble-open');
- 	});
+	 		$this.parent().css('left', + horiz_distance_to_left+ 'px');
+	 		$this.addClass('has-bubble-open');
+	 	}
+	 		$this.siblings().removeClass('has-bubble-open');
+	 	}); // end of $('.face').on('click', function(){...}
+ } // mentoring_bubble_click()
+ 
+ $('.face:nth-child(3)').addClass('has-bubble-open');
+
+// ========= /MENTORING SECTION ==========
 
 
 
- }
 
+// ========= YOUTUBE SECTION ==========
  $(window).scroll(function(){
  	scrolling_thumbnails_in_YouTube_section();
  });
@@ -30,3 +44,4 @@
 
  	$('.video-strip').css('background-position', 'center '+ -scroll_position + 'px');
  }
+// ========= /YOUTUBE SECTION ==========
